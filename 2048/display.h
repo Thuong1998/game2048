87da7c090716ccb636c7f4fe2a9fcd4910c05f72
Thuong1998 <37398323+Thuong1998@users.noreply.github.com>
2018-05-11 21:30:59 +0700

    Delete display.h

--- 2048/display.h
+++
@@ -1,9 +0,0 @@
-#ifndef DISPLAY_H
-#define DISPLAY_H
-
-void show_Game(); // vẽ khung tro choi va huong dã tro choi
-void set_Color(int value);  //ham  cai dạt mau cho tung gia tr�
-void refill_Matrix(int boardGame[][4]);   // ham dien cac gia trị cua mang vao o
-void refill_Score(int &score);   // in ra diem cua tro chơi
-
-#endif // DISPLAY_H
